30c57da1f919e7062fad3654f11c8249a8b61ad0
Your Name <aphidia@hotmail.com>
2010-12-08 15:22:40 +0000

    added simplistic minimal client.

---
+++ simpl.py
@@ -0,0 +1,112 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide import QtWebKit
+import hand2
+import common
+
+class TableWindow(QMainWindow):
+    def __init__(self):
+        super(TableWindow, self).__init__()
+
+        playerList = QTreeWidget()
+        playerList.setColumnCount(2)
+        playerList.setHeaderLabels(['Nickname', 'Stack'])
+        #playerList.header().hide()
+        players = []
+
+        andy = QTreeWidgetItem(['andy', '80'])
+        players.append(andy)
+
+        Chisness = QTreeWidgetItem(['Chisness', '112'])
+        players.append(Chisness)
+
+        WizardOfAhns = QTreeWidgetItem(['WizardOfAhns', '98'])
+        players.append(WizardOfAhns)
+
+        DrawingFishe = QTreeWidgetItem(['DrawingFishe', '120'])
+        players.append(DrawingFishe)
+
+        genjix = QTreeWidgetItem(['genjix', '100'])
+        players.append(genjix)
+
+        JimiThing34 = QTreeWidgetItem(['Jimithing34', '100'])
+        players.append(JimiThing34)
+
+        """button = QPushButton('Hello')
+        buttonItem = QTreeWidgetItem(['Hello'])
+        buttonSubItem = QTreeWidgetItem()
+        buttonSubItem.setItemWidget(button)
+        buttonItem.addChild(buttonSubItem)
+        players.append(buttonItem)"""
+
+        playerList.addTopLevelItems(players)
+
+        playerNotesSplitter = QSplitter()
+        playerNotesSplitter.setOrientation(Qt.Vertical)
+        playerNotesSplitter.addWidget(playerList)
+
+        notesSection = QWidget()
+        notesVbox = QVBoxLayout(notesSection)
+        notesVbox.addWidget(QLabel('DrawingFishe'))
+        notes = QTextEdit()
+        notes.setHtml('This player is a <i>real</i> <b>donk</b>!<br /><h1>NEVER BLUFF HIM!!</h1>')
+        notesVbox.addWidget(notes)
+        playerNotesSplitter.addWidget(notesSection)
+
+        handhist = QtWebKit.QWebView()
+        import hand
+        handhist.setHtml(hand2.handhistory)
+
+        #-----------------
+        self.wgt = QWidget()
+        hbox = QHBoxLayout(self.wgt)
+        hbox.addStretch()
+        fold = QPushButton('Fold')
+        fold.setObjectName('FoldBtn')
+        fold.setCheckable(True)
+        fold.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        hbox.addWidget(fold)
+        call = QPushButton('Call\n$1')
+        call.setObjectName('CallBtn')
+        #call.setStyleSheet('background-color: #1169a4; width: 80px; font-size: 10pt; font-weight: bold; color: white;')
+        call.setCheckable(True)
+        call.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        hbox.addWidget(call)
+        hbox.addSpacing(20)
+        rai = QPushButton('Raise\n$2')
+        rai.setObjectName('RaiseBtn')
+        #rai.setStyleSheet('background-color: #1169a4; color: #ddf; width: 80px; height: 50px; font-size: 10pt; font-weight: bold;')
+        rai.setCheckable(True)
+        rai.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        hbox.addWidget(rai)
+        self.edit = QLineEdit()
+        self.edit.setValidator(QDoubleValidator(0.0, -1.0, 2, self.edit))
+        self.edit.setMaximumSize(60,28)
+        self.edit.setText('1')
+        if QDir.setCurrent('./data/gfx/table/default/'):
+            self.wgt.setStyleSheet(str(common.loadStyleSheet('style.css')))
+        QDir.setCurrent('../../../../')
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setTickPosition(self.slider.TicksBelow)
+        sliderLayout = QHBoxLayout()
+        sliderLayout.addWidget(self.slider)
+        sliderLayout.addWidget(self.edit)
+
+        mainView = QWidget()
+        mainViewLayout = QVBoxLayout(mainView)
+        mainViewLayout.addWidget(handhist)
+        mainViewLayout.addLayout(sliderLayout)
+        mainViewLayout.addWidget(self.wgt)
+
+        mainPanelSplitter = QSplitter(self)
+        #mainPanelSplitter.addWidget(QPushButton('Hello'))
+        mainPanelSplitter.addWidget(playerNotesSplitter)
+        mainPanelSplitter.addWidget(mainView)
+        self.setCentralWidget(mainPanelSplitter)
+        self.show()
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    table = TableWindow()
+    sys.exit(app.exec_())
